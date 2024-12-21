4097716cc459895ac4413557623eb1d42be55d75
viiccwen <viiccwen@gmail.com>
2024-12-21 15:31:53 +0800

    feat: launch page
    1. add emoji-picker components
    2. todo: transaction

---
+++ frontend/src/components/customs/emoji-picker.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/utils";
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
+import { useState } from "react";
+
+interface EmojiStorePickerProps {
+  className?: string;
+  value: string | null;
+  onChange: (emoji: string) => void;
+}
+
+export const EmojiStorePicker = (props: EmojiStorePickerProps) => {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
+  const onEmojiClick = (emoji: EmojiClickData) => {
+    props.onChange(emoji.emoji);
+    setShowEmojiPicker(false);
+  };
+
+  return (
+    <div className={cn("relative", props.className)}>
+      <button
+        className="w-full text-5xl rounded-lg shadow-lg h-[220px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+        onClick={(e) => {
+          e.preventDefault();
+          setShowEmojiPicker(!showEmojiPicker);
+        }}
+      >
+        {props.value}
+      </button>
+      {showEmojiPicker && (
+        <div className="absolute top-0 left-[110%] z-50">
+          <EmojiPicker
+            onEmojiClick={(emoji: EmojiClickData) => onEmojiClick(emoji)}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
--- frontend/src/pages/launch.tsx
+++ frontend/src/pages/launch.tsx
@@ -1,5 +1,114 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { EmojiStorePicker } from "@/components/customs/emoji-picker";
+import { LaunchFormSchema, LaunchFormSchemaType } from "@/utils/type";
+import { useWriteContract } from "wagmi";
 
 export default function Launch() {
-  return <>{String.fromCodePoint(0x1f600)}</>;
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+    control,
+  } = useForm<LaunchFormSchemaType>({
+    resolver: zodResolver(LaunchFormSchema),
+  });
+
+  // 取得 emojiCount
+  const {
+    data: emojiCountData,
+    isLoading: isCountLoading,
+    isError: isCountError,
+    error: countError,
+  } = useWriteContract({
+    ...SmartContractConfig,
+    functionName: "getEmojiCount",
+  });
+
+  const onSubmit = (formdata: LaunchFormSchemaType) => {
+    console.log(formdata);
+  };
+
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold">
+          Launch your emoji 🚀
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* emoji picker */}
+          <Controller
+            name="emoji"
+            control={control}
+            render={({ field }) => (
+              <EmojiStorePicker
+                value={field.value}
+                onChange={(emoji: string) => setValue("emoji", emoji)}
+              />
+            )}
+          />
+          {errors.emoji && (
+            <span className="text-red-500 text-xs">{errors.emoji.message}</span>
+          )}
+
+          <div className="grid w-full items-center gap-4 mt-3">
+            {/* Price Input */}
+            <div className="flex flex-col items-start space-y-1.5">
+              <Label htmlFor="price" className="text-base">
+                金額 (wei)
+              </Label>
+              <Input
+                type="number"
+                defaultValue={100}
+                placeholder="請輸入..."
+                {...register("price", { valueAsNumber: true })}
+              />
+              {errors.price && (
+                <span className="text-red-500 text-sm">
+                  {errors.price.message}
+                </span>
+              )}
+            </div>
+
+            {/* Description Input */}
+            <div className="flex flex-col items-start space-y-1.5">
+              <Label htmlFor="description" className="text-base">
+                介紹
+              </Label>
+              <Textarea
+                id="description"
+                placeholder="請輸入..."
+                {...register("description")}
+              />
+              {errors.description && (
+                <span className="text-red-500 text-sm">
+                  {errors.description.message}
+                </span>
+              )}
+            </div>
+          </div>
+
+          {/* Submit Button */}
+          <div className="w-ful mt-5">
+            <Button
+              variant="outline"
+              type="submit"
+              className="duration-300 hover:text-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 w-full"
+            >
+              Launch 🚀
+            </Button>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
 }
